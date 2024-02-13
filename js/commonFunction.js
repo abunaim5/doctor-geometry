6ed54566e6ff3441051e69fbf759aa99144f9cd2
abunaim5 <abunaim140@gmail.com>
2024-02-13 19:43:20 +0600

    added set elements innerText function

--- js/commonFunction.js
+++ js/commonFunction.js
@@ -1,6 +1,14 @@
+// Get inputs value
 function getInputValueById(inputId) {
     const getInput = document.getElementById(inputId);
     const getInputStrValue = getInput.value;
     const convertStrValueToNumber = parseFloat(getInputStrValue);
     return convertStrValueToNumber;
+}
+// Set elements inner text
+function setInnerTextById(elementId, area) {
+    const getElement = document.getElementById(elementId);
+    console.log(getElement);
+    const setInnerText = getElement.innerText = area;
+    return setInnerText;
 }
--- js/triangle.js
+++ js/triangle.js
@@ -2,5 +2,5 @@ function calculateTriangleArea() {
     const triangleBase = getInputValueById('triangle-base');
     const triangleHeight = getInputValueById('triangle-height')
     const triangleArea = 0.5 * triangleBase * triangleHeight;
-    console.log(triangleArea);
+    setInnerTextById('triangle-area', triangleArea);
 }
